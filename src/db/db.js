5ca28c4ecb0084df0bc764d099110e30860b084e
ir-khan <besttime444444@gmail.com>
2025-03-09 17:26:25 +0500

    Config env

--- src/db/db.js
+++ src/db/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(
-            `${process.env.MONGO_URI}/moodeats`
+            `${process.env.MONGODB_URI}/moodeats`
         );
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
